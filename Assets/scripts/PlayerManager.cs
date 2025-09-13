82096b417e783eee86b4f94f8a545df834bf3807
DylanManley <116343216+DylanManley@users.noreply.github.com>
2025-09-13 16:56:03 +0100

    Demo ready

--- Assets/scripts/PlayerManager.cs
+++ Assets/scripts/PlayerManager.cs
@@ -7,11 +7,9 @@ public class PlayerManager : MonoBehaviour
 {
     public PlayerController Dylan;
     bool DylanActive = true;
-    [SerializeField] private GameObject DylanWeapon;
 
     public PlayerController Darragh;
     bool DarraghActive = false;
-    [SerializeField] private GameObject DarraghWeapon;
     private bool canPlay = false;
     public bool canSwitch = true;
 
@@ -38,8 +36,6 @@ public class PlayerManager : MonoBehaviour
             Vector3 changedPos = new Vector3(Darragh.transform.position.x, Darragh.transform.position.y + 0.8f, Darragh.transform.position.z);
             UIcam.transform.SetPositionAndRotation(changedPos, Darragh.transform.rotation);
             UIcam.transform.SetParent(Darragh.transform);
-            DarraghWeapon.SetActive(true);
-            DylanWeapon.SetActive(false);
 
             DylanActive = false;
             Dylan.Deactivate();
@@ -51,8 +47,6 @@ public class PlayerManager : MonoBehaviour
             Vector3 changedPos = new Vector3(Dylan.transform.position.x, Dylan.transform.position.y + 0.8f, Dylan.transform.position.z);
             UIcam.transform.SetPositionAndRotation(changedPos, Dylan.transform.rotation);
             UIcam.transform.SetParent(Dylan.transform);
-            DarraghWeapon.SetActive(false);
-            DylanWeapon.SetActive(true);
 
             DarraghActive = false;
             Darragh.Deactivate();
@@ -75,8 +69,6 @@ public class PlayerManager : MonoBehaviour
             Vector3 changedPos = new Vector3(Darragh.transform.position.x, Darragh.transform.position.y + 0.8f, Darragh.transform.position.z);
             UIcam.transform.SetPositionAndRotation(changedPos, Darragh.transform.rotation);
             UIcam.transform.SetParent(Darragh.transform);
-            DarraghWeapon.SetActive(true);
-            DylanWeapon.SetActive(false);
 
             DylanActive = false;
             Dylan.Deactivate();
@@ -91,8 +83,6 @@ public class PlayerManager : MonoBehaviour
             Vector3 changedPos = new Vector3(Dylan.transform.position.x, Dylan.transform.position.y + 0.8f, Dylan.transform.position.z);
             UIcam.transform.SetPositionAndRotation(changedPos, Dylan.transform.rotation);
             UIcam.transform.SetParent(Dylan.transform);
-            DarraghWeapon.SetActive(false);
-            DylanWeapon.SetActive(true);
 
             DarraghActive = false;
             Darragh.Deactivate();
